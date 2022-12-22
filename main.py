7475417a4b6408edf95c365a4f82515070dc5ce8
sarah bagheri <95619163+Sarahbaqeri@users.noreply.github.com>
2022-12-21 16:30:36 -0800

    modified main file
    find class type of objects and use isinstance method to find out the class method

--- main.py
+++ main.py
@@ -2,4 +2,17 @@
 from CRUD import Products
 
 p1=Products(1,1,'sarah','saaa','asqs','qs','wsq',True,'ws',11,221,21,'sqws','qwsx',True,12,43)
+
 print(p1)
+
+print(type(Products))
+
+print(p1.create())
+
+print(p1.read())
+
+print(p1.update())
+
+print(p1.delete)
+
+print(isinstance(p1,Products))
